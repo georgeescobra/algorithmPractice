cb331026d197e8e52ebe1f51622fe1cd97500407
George <tgeorge.escobar@gmail.com>
2024-03-24 14:56:31 -0700

    add test case

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -1,5 +1,14 @@
 class AdjacencyList:
-  pass
+  test_case_0 = [
+    [0, 1, 1, 1], # 0th node connected to 1th, 2th, 3th node
+    [1, 0, 1, 0], # 1th node connected to 0th, 2th node
+    [1, 1, 0, 0], # 2th node connecetd to 0th, 2th node
+    [1, 0, 0, 0], # 3th node connected t0 0th node
+  ]
+  
+
+  def __init__(self):
+    pass
 
 class AdjacencyMatrix:
   pass
